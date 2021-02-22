edb97d6c17c4b6eacf754ff03d28f009251c0ee7
JennyM0715 <jennifer.martinez.754@my.csun.edu>
2021-02-21 19:55:03 -0800

    fixing display

--- script.js
+++ script.js
@@ -24,12 +24,12 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
       //var obj = { property_1:   value_1,   // property_# may be an identifier...
      //             property_2:   value_2
     //             }; 
-    //trying to fix display
+    //trying to fix display of name and discipline attribute
     var pet_info = {
-      name: Stryker, 
+      name: "Stryker", 
       weight: "12", 
       happiness: "5",
-      discipline: "3",  //added the new behavior and set initial value
+      discipline: 3,  //added the new behavior and set initial value
     };
   
 
